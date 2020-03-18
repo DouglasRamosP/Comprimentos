930bbe901e53f865e1674eec43347f7fe421d0ae
DouglasRamosP <43833349+DouglasRamosP@users.noreply.github.com>
2020-03-18 00:08:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgRelogio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Relogio R = new Relogio(7200);
+           // R.Segundos = 7200;
+
+
+            double ss = R.Segundos;
+            System.Console.WriteLine(ss);
+            System.Console.WriteLine("SEGUNDOS:");
+
+
+
+           
+            double hh = R.Hora;
+            System.Console.WriteLine(hh);
+            System.Console.WriteLine("HORAS");
+            System.Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Relogio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgRelogio
+{
+    class Relogio
+    {
+
+        private double segundos;
+
+        /*Contrutor*/
+
+        public Relogio(double seg)
+        {
+            segundos = seg;
+        }
+
+
+        public double Segundos
+        {
+            get { return segundos; }
+            set { segundos = value; }
+
+        }
+
+        public double Hora
+        {
+            get { return segundos / 3600; }
+            set {if (value <= 1 && value >= 24)
+                    System.Console.WriteLine("Valor deve ser entre 1 e 24");
+                segundos = value * 3600; }
+        }
+
+    }
+}
